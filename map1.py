36c63b8aa2bd18b16e4a2134869a7a9d04b709fd
Mousam Dhakal <Mousamdhakal7.md@gmail.com>
2018-10-08 20:36:03 +0545

    Updated content to show some location's info

--- map1.py
+++ map1.py
@@ -1,9 +1,16 @@
 import folium
+import pandas
 map = folium.Map(location = [27.672480,85.378214],zoom_start=100)
-
+pop = []
+data = pandas.read_csv("places.csv")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+pop = list(data["PLACE"])
 
 fg = folium.FeatureGroup(name="My map")
-fg.add_child(folium.Marker(location = [26.669453,87.382813], popup ="This is home", icon=folium.Icon(color='green')))
+for i,j,k in zip(lat,lon,pop):
+    fg.add_child(folium.Marker(location = [i,j], popup = k , icon=folium.Icon(color='green')))
+
 
 map.add_child(fg)
 
